004c981f0446d0b2642d5c3b5e511a58407c3ee1
Marcelo Munhoz <me@marcelomunhoz.com>
2025-05-06 15:01:16 -0300

    Middleware path fix

--- app/src/utils/usefulFunctions.js
+++ app/src/utils/usefulFunctions.js
@@ -1,28 +1,29 @@
 import { ref, reactive, watch } from "vue";
 
-const evtBus = reactive({})
+const evtBus = reactive({});
 const data = ref(Object);
 
 export function emit(evtName, payload) {
-  evtBus[evtName] = payload
+  evtBus[evtName] = payload;
 }
 
 export function on(evtName, callback) {
-  watch(() => evtBus[evtName], newValue => {
-    if (newValue !== undefined) {
-      callback(newValue)
+  watch(
+    () => evtBus[evtName],
+    (newValue) => {
+      if (newValue !== undefined) {
+        callback(newValue);
+      }
     }
-  })
+  );
 }
 
 async function fetchData(apiParams) {
-  const API_BASE_URL = import.meta.env.PROD
-    ? 'https://flickr-public-photos.onrender.com'
-    : '/api';
+  const API_BASE_URL = import.meta.env.PROD ? "https://flickr-public-photos.onrender.com/api" : "/api";
 
   let url = `${API_BASE_URL}/flickr`;
 
-  if (apiParams && typeof apiParams === 'object') {
+  if (apiParams && typeof apiParams === "object") {
     const queryParams = new URLSearchParams(apiParams).toString();
     url += `?${queryParams}`;
   }
@@ -32,23 +33,20 @@ async function fetchData(apiParams) {
   return data;
 }
 
-
-
-
 function theDate(dte) {
   const date = new Date(dte);
   const opt = { year: "numeric", month: "long", day: "numeric" };
   return date.toLocaleString("en-US", opt);
 }
 
-function bordered (state, target = null, url = null)  {
+function bordered(state, target = null, url = null) {
   if (state && target && url) {
     target.classList.add("border");
   } else if (!state && target && url) {
     target.firstElementChild.classList.remove("border");
   }
-};
+}
 
 // Export each function as a named export
 export { fetchData, theDate, bordered };
-export default { emit, on }
+export default { emit, on };
